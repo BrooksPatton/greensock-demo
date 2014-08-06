a4c9a8c0209ff11c2aa4fc95dd142861a0ea6ee2
PCLongmont <PCLongmont@gmail.com>
2014-08-06 17:01:23 -0600

    icons linked locally, simple css size animation

--- patrick/main.js
+++ patrick/main.js
@@ -1,3 +1,22 @@
+
 $(document).on('ready', function() {
-  
+
+
+var animateIn = function() {
+	return TweenLite.to($('.little-bit'), 2, {'font-size': '400px'});
+};
+
+var animateOut = function() {
+	return TweenLite.to($('.little-bit'), 2, {'font-size': '100px'});
+};
+
+var tl = new TimelineLite();
+tl.add(animateIn()).add(animateOut());
+
+
+tl.play();
+
+
+
+
 });
